2366338081c4579492bd4dd7f50da32196c2ce64
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-04-06 18:40:58 +0900

    vuetify for typescript

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat @click="show">Introduction</v-btn>
+      <v-btn flat>Author Details</v-btn>
+      <v-btn flat>Contacts</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
 
     <v-content>
       <HelloWorld/>
+      <router-link to="author">author</router-link>
     </v-content>
   </v-app>
 </template>
