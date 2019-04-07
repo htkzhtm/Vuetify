55e6b375af323a647e2415159d93fb8123e94fb5
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-04-07 13:57:23 +0900

    Add author page

---
+++ src/components/AuthorDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/author.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style scoped>
+
+</style>
