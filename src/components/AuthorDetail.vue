7c96431d03d5c09a74dc77867f1e917448c0848e
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-04-10 20:10:50 +0900

    add author introduction page

--- src/components/AuthorDetail.vue
+++ src/components/AuthorDetail.vue
@@ -8,13 +8,17 @@
         <v-avatar size="256px">
           <v-img
             :src="require('../assets/author.png')"
-            class="my-3"
-            contain
-            height="200"
             alt="Avatar"
           ></v-img>
         </v-avatar>
       </v-flex>
+      <v-card-text class="text-xs-center">
+        <h1 class="category text-gray font-weight-thin mb-3">Hitomi Hatsukaze</h1>
+        <p class="card-description font-weight-light">A web developer in Japan. Recently interested in using Vue.js, and Laravel for development. Get along well. Thank you.</p>
+        <template v-for="(item, index) in items">
+
+        </template>
+      </v-card-text>
     </v-layout>
   </v-container>
 </template>
@@ -22,7 +26,20 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    
+  data () {
+    return {
+      items: [
+        {
+          title: 'Name',
+          description: 'Hatsukaze Hitomi'
+        },
+        {
+          title: 'gender',
+          description: 'male'
+        }
+      ]
+    }
+  }
 })
 </script>
 
