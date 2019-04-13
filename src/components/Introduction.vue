fa6a9050639f831bf22660a93c634b187545b5ef
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-04-13 10:09:06 +0900

    fix introduction use v-for derective

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -2,17 +2,36 @@
   <section id="introduction">
     <h1 class="title-head">Introduction of a nice detective Conan</h1>
     <div class="layout features-container row wrap">
-      <div class="flex feature-container xs12">
-        <div class="layout row wrap">
-          <div class="flex feature xs12 md6">
-            <h2 class="sub-title">The national animation in Japan</h2>
-            <p class="description">Detective Conan is one of the major animations in Japan. Anime is broadcasted every Saturday.</p>
+      <div class="content flex feature-container xs12"
+        v-for="(introduction, index) in introductions"
+        :key="introduction.subTitle"
+      >
+        <div v-if="index%2 == 0">
+          <div class="layout row wrap">
+            <div class="flex feature xs12 md6">
+              <h2 class="sub-title">{{ introduction.subTitle }}</h2>
+              <p class="description">{{ introduction.description }}</p>
+            </div>
+            <div class="flex pic hidden-sm-and-down md6">
+              <img :src="introduction.image" class="img-large">
+            </div>
+            <div class="flex pic xs12 hidden-md-and-up">
+              <img :src="introduction.image" class="img-small">
+            </div>
           </div>
-          <div class="flex pic hidden-sm-and-down md6">
-            <img src="../assets/conan.jpeg" class="img-large">
-          </div>
-          <div class="flex pic xs12 hidden-md-and-up">
-            <img src="../assets/conan.jpeg" class="img-small">
+        </div>
+        <div v-if="index%2 == 1">
+          <div class="layout row wrap">
+            <div class="flex pic hidden-sm-and-down md6">
+              <img :src="introduction.image" class="img-large">
+            </div>
+            <div class="flex feature xs12 md6">
+              <h2 class="sub-title">{{ introduction.subTitle }}</h2>
+              <p class="description">{{ introduction.description }}</p>
+            </div>
+            <div class="flex pic xs12 hidden-md-and-up">
+              <img :src="introduction.image" class="img-small">
+            </div>
           </div>
         </div>
       </div>
@@ -25,7 +44,28 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  name: 'introduction'
+  name: 'introduction',
+  data () {
+    return {
+      introductions: [
+        {
+          subTitle: "The national animation in Japan",
+          description: "Detective Conan is one of the major animations in Japan. Anime is broadcasted every Saturday.",
+          image: require("../assets/conan.jpeg")
+        },
+        {
+          subTitle: "The main character is a high school student with a shrinking body!?",
+          description: "The true name of Edogawa Conan is Kudo Shinichi. He was taken APTX4869 by somebody and his body shrank. Such is the story he solves many cases.",
+          image: require("../assets/shinichi.jpg")
+        },
+        {
+          subTitle: "Love and Comedy",
+          description: "This work not only solves the case, but also specializes in love and codedy. Therefore it is also supported by women.",
+          image: require("../assets/ran.jpg")
+        },  
+      ]
+    }
+  }
 })
 </script>
 
@@ -70,5 +110,10 @@ export default Vue.extend({
 .layout {
   margin-top: 20px;
 }
+
+.content {
+  margin-top: 20px;
+}
+
 </style>
 
